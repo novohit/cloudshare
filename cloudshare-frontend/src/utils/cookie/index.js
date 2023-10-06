45be94ccb64d84136756ca924ad2a3b0957cfcad
novohit <zwx_info@163.com>
2023-10-06 17:50:27 +0800

    feat: add frontend

---
+++ cloudshare-frontend/src/utils/cookie/index.js
@@ -0,0 +1,39 @@
+'use strict'
+
+import cookie from 'vue-cookie'
+
+const LOGIN_TOKEN = 'login_token',
+    SHARE_TOKEN = 'share_token',
+    EMPTY_STR = ''
+
+export function setToken(token) {
+    cookie.set(LOGIN_TOKEN, token)
+}
+
+export function getToken() {
+    let token = cookie.get(LOGIN_TOKEN)
+    if (token) {
+        return token
+    }
+    return EMPTY_STR
+}
+
+export function clearToken() {
+    cookie.delete(LOGIN_TOKEN)
+}
+
+export function setShareToken(token) {
+    cookie.set(SHARE_TOKEN, token)
+}
+
+export function getShareToken() {
+    let token = cookie.get(SHARE_TOKEN)
+    if (token) {
+        return token
+    }
+    return EMPTY_STR
+}
+
+export function clearShareToken() {
+    cookie.delete(SHARE_TOKEN)
+}
