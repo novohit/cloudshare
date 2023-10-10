c0b0c2908785bc8a11ecd42005de104642ff4ad7
novohit <zwx_info@163.com>
2023-10-10 12:15:51 +0800

    feat: 文件存储引擎架构

---
+++ framework/storage-spring-boot-starter/storage-engine-core/src/main/java/com/cloudshare/storage/core/StorageEngine.java
@@ -0,0 +1,8 @@
+package com.cloudshare.storage.core;
+
+/**
+ * @author novo
+ * @since 2023/10/10
+ */
+public interface StorageEngine {
+}
---
+++ framework/storage-spring-boot-starter/storage-engine-oss/src/main/java/com/cloudshare/storage/oss/OSSStorageEngine.java
@@ -0,0 +1,10 @@
+package com.cloudshare.storage.oss;
+
+import com.cloudshare.storage.core.AbstractStorageEngine;
+
+/**
+ * @author novo
+ * @since 2023/10/10
+ */
+public class OSSStorageEngine extends AbstractStorageEngine {
+}
